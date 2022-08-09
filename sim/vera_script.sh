0ff6eb9e7c2d3ab15f975d5e4aa55bb33b8e1a60
Albert Zhang <acz2122@columbia.edu>
2022-08-09 08:58:41 -0400

    vera script update

--- sim/vera_script.sh
+++ sim/vera_script.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#SBATCH -A C3SE508-18-3 -p vera
+#SBATCH -A C3SE508-18-3 -p astro1
 #SBATCH -J <<<JOB_NAME>>>
 #SBATCH -N 1 --exclusive
-#SBATCH -t 0-24:00:00
+#SBATCH -t 1-12:00:00
 
 # flat_modules
 # module load foss/2021b SciPy-bundle/2021.10-foss-2021b matplotlib/3.4.3-foss-2021b
@@ -14,11 +14,11 @@ source ~/bert-vera.sh
 DIRS=($(ls -d */))
 
 # Fetch one directory from the array based on the task ID (index starts from 0)
-CURRENT_DIR=${DIRS[$SLURM_ARRAY_TASK_ID]}
 
-echo "Running simulation $CURRENT_DIR"
-
-# Go to folder
-cd $CURRENT_DIR
-
-/usr/bin/env bash exec.sh
+for CURRENT_DIR in $DIRS; do
+    echo "Running simulation $CURRENT_DIR"
+    cd $CURRENT_DIR
+    srun -n 1 exec.sh &
+    cd ..
+done
+wait
